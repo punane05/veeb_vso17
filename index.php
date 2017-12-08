ae4739e679eb813353ac93606c71160583a4ecc0
Anna Karutina <anna.karutina@khk.ee>
2017-12-08 09:28:07 +0200

    Kirjeldatud kommentaaride kasutamine

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anna.karutina
+ * Date: 08.12.2017
+ * Time: 9:03
+ */
+# see on ka üherealine kommentaar
+// üherealine kommentaar
+/**
+ * mitme-
+ * realine-
+ * kommentaar
+ */
+?>
+
