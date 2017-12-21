c37b00ca0c9fa0513f9b8a9043a302dae2cd7dd8
Anna Karutina <anna.karutina@khk.ee>
2017-12-21 14:23:29 +0200

    Massiivi loomine ja väärtuste lisamine

---
+++ massiivid.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anna.karutina
+ * Date: 21.12.2017
+ * Time: 14:19
+ */
+$arvud = array(); // tühi massiiv
+// lisame väärtused
+$arvud[] = 5;
+$arvud[] = 1;
+$arvud[] = 4;
+$arvud[] = 2;
+$arvud[] = 3;
+
+// kuidas luua juba väärtustega massiivi
+$arvud1 = array(1, 2, 3, 4, 5);
+
