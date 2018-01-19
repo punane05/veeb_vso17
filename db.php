bf4f90d7017d930ce62c36a312ef563acc30f033
Anna Karutina <anna.karutina@khk.ee>
2018-01-19 13:30:36 +0200

    Koostatud päringu saatmise funktsioon

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anna.karutina
+ * Date: 19.01.2018
+ * Time: 13:05
+ */
+
+// nõuame andmebaasitöötlusfunktsioonid kasutusele
+require_once 'db_fnk.php';
+
+// tekitame ühendus andmebaasiga
+$dbYhendus = yhendus();
